20e82bdb3bb8077bffe53976da221351a8a940c4
naomi.fukazawa <velphedia@yahoo.co.jp>
2020-05-19 06:59:04 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,9 +20,15 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+        $posts = $user->posts()->orderBy('created_at', 'desc')->paginate(10);
         
-        return view('users.show', [
+        $data = [
             'user' => $user,
-        ]);
+            'posts' => $posts,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.show', $data);
     }
 }
