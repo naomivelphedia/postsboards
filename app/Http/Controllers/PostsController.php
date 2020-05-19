b6546fa6108b636591f3d92ec2316834dafa6790
naomi.fukazawa <velphedia@yahoo.co.jp>
2020-05-19 23:15:17 +0000

    timeline

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,14 +11,13 @@ class PostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $posts = $user->posts()->orderBy('created_at', 'desc')->paginate(10);
+            $posts = $user->feed_posts()->orderBy('created_at', 'desc')->paginate(10);
             
             $data = [
                 'user' => $user,
                 'posts' => $posts,
             ];
         }
-        
         return view('welcome', $data);
     }
     
